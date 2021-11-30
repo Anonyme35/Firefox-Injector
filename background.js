d9e30e6aa19a73d25d0aeadfb3af733f5c77f875
anonyme <remi.assidi@gmail.com>
2021-11-30 15:55:21 +0100

    Correct XSS

--- background.js
+++ background.js
@@ -10,6 +10,6 @@ browser.menus.onClicked.addListener((info, tab) => {
     browser.tabs.executeScript(tab.id, {
       frameId: info.frameId,
       code: `elem = browser.menus.getTargetElement(${info.targetElementId});
-      elem.value="<img src=1 onerror=alert(" + window.location.href + " Variable: " + elem.id + ")>"`,
+      elem.value="<img src=1 onerror=alert("\" + window.location.href + " Variable: " + elem.id + \"")>"`,
     });
 });
